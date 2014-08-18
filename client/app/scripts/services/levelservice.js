cc7658b7ad66cfa9757e44f53d5aa10528c3a68a
Simon Wendel <mail@simonwendel.se>
2014-08-18 16:16:01 +0200

    Add the LevelService to the client

---
+++ client/app/scripts/services/levelservice.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('clientApp')
+  .service('LevelService', function LevelService() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
---
+++ client/test/spec/services/levelservice.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('Service: LevelService', function () {
+
+  // load the service's module
+  beforeEach(module('clientApp'));
+
+  // instantiate service
+  var LevelService;
+  beforeEach(inject(function (_LevelService_) {
+      LevelService = _LevelService_;
+  }));
+
+  it('should do something', function () {
+    expect(!!LevelService).toBe(true);
+  });
+
+});

84f57eb491e1e5561fb9fa538761865e6bd1f0c0
Simon Wendel <mail@simonwendel.se>
2014-08-19 13:16:46 +0200

    Fix error in unit test of toBeJsonEqual matcher
    The unit test file for the custom Jasmine matcher toBeJsonEqual had
    two different errors in it that made the check for .not.toEqual and
    .not.toBeJsonEqual to be completely non-functional.

--- client/test/matchers/spec/toBeJsonEqual.js
+++ client/test/matchers/spec/toBeJsonEqual.js
@@ -1,26 +1,24 @@
 'use strict';
 
 describe('Test utility: toBeJsonEqual custom jasmine matcher', function () {
-
-    var firstObject;
-    var secondObject;
-
     var NotObject = function(one, two) {
-        this.one = 1;
-        this.two = 2
+        this.one = one;
+        this.two = two;
     };
 
     it('should return true if the objects render to the same JSON string representation', function () {
-        firstObject = new NotObject(1, 2);
-        secondObject = { one : 1, two : 2 };
+        var firstObject = new NotObject(1, 2);
+        var secondObject = { one : 1, two : 2 };
 
         expect(firstObject).toEqual(secondObject);
+        expect(firstObject).toBeJsonEqual(secondObject);
     });
 
     it('should return false if the objects does not render to the same JSON string representation', function () {
-        firstObject = new NotObject(3, 2);
-        secondObject = { one : 1, two : 2 };
+        var firstObject = new NotObject(3, 2);
+        var secondObject = { one : 1, two : 2 };
 
-        expect(firstObject).toEqual(secondObject);
+        expect(firstObject).not.toEqual(secondObject);
+        expect(firstObject).not.toBeJsonEqual(secondObject);
     });
 });

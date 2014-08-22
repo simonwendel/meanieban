3d551322cc32cddf6b16f9b8803337ac3a2c6c28
Simon Wendel <mail@simonwendel.se>
2014-08-22 11:57:01 +0200

    Add test/matchers/*.js to code coverage analysis

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -62,7 +62,8 @@ module.exports = function (config) {
             // source files, that you wanna generate coverage for
             // do not include tests or libraries
             // (these files will be instrumented by Istanbul)
-            'app/scripts/**/*.js': ['coverage']
+            'app/scripts/**/*.js': ['coverage'],
+            'test/matchers/*.js': ['coverage']
         },
 
         // Continuous Integration mode

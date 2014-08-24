1248e8e5d5182ffd9cd5c4474bfbae56466deec8
Simon Wendel <mail@simonwendel.se>
2014-08-24 20:58:53 +0200

    Extracted the level data from the levelCollection
    It now uses the global levelData value instead and operates on that one.

--- app/scripts/services/levelcollection.js
+++ app/scripts/services/levelcollection.js
@@ -1,52 +1,18 @@
 'use strict';
 
 angular.module('meanieBanApp')
-    .factory('levelCollection', function LevelCollectionFactory() {
-        var collection = [];
-
-        // test level 1
-        collection.push(
-            {id: -1, width: 27, height: 13, name: 'test level 1', rows: [
-                [7, 7, 7, 7, 7],
-                [7, 1, 1, 1, 7],
-                [7, 1, 1, 1, 7],
-                [7, 7, 7, 7, 7, 1, 1, 1, 1, 1, 7, 1, 1, 1, 7, 7, 7, 7, 7, 7, 7],
-                [7, 1, 1, 1, 7, 7, 7, 7, 7, 1, 7, 1, 1, 1, 2, 2, 2, 2, 2, 1, 7],
-                [7, 7, 7, 7, 7, 1, 7, 1, 7, 7, 1, 1, 7, 1, 7, 1, 1, 1, 1, 1, 7, 1, 7, 1, 7],
-                [7, 1, 4, 1, 4, 1, 4, 1, 4, 1, 4, 1, 7, 1, 7, 7, 1, 1, 1, 7, 7, 1, 4, 1, 7],
-                [7, 1, 7, 1, 7, 7, 2, 2, 2, 2, 2, 2, 7, 7, 7, 7, 1, 7, 7, 7, 1, 4, 4, 1, 7, 7, 7],
-                [7, 1, 1, 1, 1, 1, 1, 7, 7, 1, 3, 1, 7, 1, 1, 1, 1, 7, 1, 7, 1, 1, 4, 4, 1, 1, 7],
-                [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 5, 4, 4, 1, 1, 1, 7, 7, 1, 7, 1, 1, 1, 1, 1, 1, 7],
-                [7, 2, 4, 1, 4, 7, 1, 7, 1, 1, 7, 7, 7, 7, 7, 7, 7, 7],
-                [7, 2, 7, 7, 1, 1, 1, 7],
-                [7, 7, 7, 7, 7, 7, 7, 7]
-            ]});
-
-        // test level 2
-        collection.push(
-            {id: -2, width: 9, height: 9, name: 'test level 2', rows: [
-                [0, 7, 7, 1, 7, 7, 7, 7, 7],
-                [7, 7, 1, 7, 7, 1, 2, 1, 7],
-                [7, 1, 7, 7, 1, 4, 2, 1, 7],
-                [7, 7, 1, 4, 1, 1, 1, 7],
-                [7, 7, 1, 4, 6, 1, 7, 7, 7],
-                [7, 1, 4, 1, 1, 7, 7],
-                [7, 2, 2, 1, 7, 7, 1, 7, 7],
-                [7, 1, 1, 1, 7, 1, 7, 7],
-                [7, 7, 7, 7, 7, 1, 7]
-            ]});
-
+    .factory('levelCollection', function LevelCollectionFactory(levelData) {
         return {
             all: function () {
-                return collection;
+                return levelData;
             },
             names: function () {
-                return collection.map(function (level) {
+                return levelData.map(function (level) {
                     return { id: level.id, name: level.name };
                 });
             },
             get: function (id) {
-                return collection.filter(function (level) {
+                return levelData.filter(function (level) {
                     return level.id == id;
                 })[0];
             }
--- test/spec/services/levelcollection.js
+++ test/spec/services/levelcollection.js
@@ -2,30 +2,40 @@
 
 describe('Factory: levelCollection', function () {
 
-    beforeEach(module('meanieBanApp'));
+    var levelCollection, levelDataMock;
+    beforeEach(
+        module('meanieBanApp', function ($provide) {
+            levelDataMock = [
+                {id: -2, width: 9, height: 9, name: 'test level 2', rows: [
+                    [0, 7, 7, 1, 7, 7, 7, 7, 7],
+                    [7, 7, 1, 7, 7, 1, 2, 1, 7],
+                    [7, 1, 7, 7, 1, 4, 2, 1, 7],
+                    [7, 7, 1, 4, 1, 1, 1, 7],
+                    [7, 7, 1, 4, 6, 1, 7, 7, 7],
+                    [7, 1, 4, 1, 1, 7, 7],
+                    [7, 2, 2, 1, 7, 7, 1, 7, 7],
+                    [7, 1, 1, 1, 7, 1, 7, 7],
+                    [7, 7, 7, 7, 7, 1, 7]
+                ]}
+            ];
 
-    var levelCollection;
+            $provide.value('levelData', levelDataMock);
+        }));
+
     beforeEach(inject(function (_levelCollection_) {
-
         levelCollection = _levelCollection_;
-
     }));
 
     describe('levelCollection.all()', function () {
 
         it('should return all levels in the levelCollection.', function () {
             var levels = levelCollection.all();
-            expect(levels.length).toBe(2);
+            expect(levels.length).toBe(1);
 
-            expect(levels[0].id).toBe(-1);
-            expect(levels[0].name).toBe('test level 1');
-            expect(levels[0].height).toBe(13);
-            expect(levels[0].width).toBe(27);
-
-            expect(levels[1].id).toBe(-2);
-            expect(levels[1].name).toBe('test level 2');
-            expect(levels[1].height).toBe(9);
-            expect(levels[1].width).toBe(9);
+            expect(levels[0].id).toBe(-2);
+            expect(levels[0].name).toBe('test level 2');
+            expect(levels[0].height).toBe(9);
+            expect(levels[0].width).toBe(9);
         });
 
     });
@@ -34,16 +44,14 @@ describe('Factory: levelCollection', function () {
 
         it('should return all level names from the levelCollection.', function () {
             var names = levelCollection.names();
-            expect(names.length).toBe(2);
-            expect(names[0].name).toBe('test level 1');
-            expect(names[1].name).toBe('test level 2');
+            expect(names.length).toBe(1);
+            expect(names[0].name).toBe('test level 2');
         });
 
         it('should return all level ids from the levelCollection.', function () {
             var names = levelCollection.names();
-            expect(names.length).toBe(2);
-            expect(names[0].id).toBe(-1);
-            expect(names[1].id).toBe(-2);
+            expect(names.length).toBe(1);
+            expect(names[0].id).toBe(-2);
         });
 
     });

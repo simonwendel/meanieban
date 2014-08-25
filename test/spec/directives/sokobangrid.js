6b1b00e29ba824954d7a3097a5d95499cc034db6
Simon Wendel <mail@simonwendel.se>
2014-08-25 18:24:58 +0200

    Give better names to scope variables in sokoban-grid tests

--- test/spec/directives/sokobangrid.js
+++ test/spec/directives/sokobangrid.js
@@ -5,28 +5,28 @@ describe('Directive: sokobanGrid', function () {
     beforeEach(module('meanieBanApp'));
     beforeEach(module('views/sokoban-grid-template.html'));
 
-    var element,
-        scope,
-        level;
+    var element, directiveScope, level;
 
     beforeEach(inject(function ($rootScope, $compile) {
-        scope = $rootScope.$new();
-
+        var pageScope = $rootScope.$new();
         level = {id: 6, width: 8, height: 1, name: 'test level 1', rows: [
             [0, 1, 2],
             [3, 4, 5]
         ]};
-        scope.level = level;
+
+        pageScope.level = level;
 
         element = angular.element('<sokoban-grid grid="level.rows"></sokoban-grid>');
-        element = $compile(element)(scope);
+        element = $compile(element)(pageScope);
 
-        scope.$digest();
-        scope = element.isolateScope();
+        pageScope.$digest();
+
+        directiveScope = element.isolateScope();
     }));
 
     it('should get some settings from the grid attribute.', function () {
-        expect(scope.grid).toBe(level.rows);
+        expect(directiveScope.grid).toBe(level.rows);
+        expect(element.html()).toBeDefined();
         expect(element.html()).not.toBe('');
     });
 });

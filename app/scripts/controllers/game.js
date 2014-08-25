9d270b4a5afef5f4b88ca95585104c35c71ae21a
Simon Wendel <mail@simonwendel.se>
2014-08-25 11:38:33 +0200

    Add the level to the GameCtrl scope

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('meanieBanApp')
-    .controller('GameCtrl', function GameCtrl($scope, $routeParams) {
+    .controller('GameCtrl', function GameCtrl($scope, $routeParams, levelCollection) {
         var currentId = $routeParams.id;
         $scope.levelId = currentId;
+        $scope.level = levelCollection.get(currentId);
     });
--- test/spec/controllers/game.js
+++ test/spec/controllers/game.js
@@ -4,19 +4,35 @@ describe('Controller: GameCtrl', function () {
 
     beforeEach(module('meanieBanApp'));
 
-    var GameCtrl, scope, routeParams;
+    var GameCtrl, scope, routeParams, levelCollection, level;
 
     beforeEach(inject(function ($controller, $rootScope) {
         scope = $rootScope.$new();
         routeParams = { id: 6 };
 
+        level = {id: 6, width: 8, height: 1, name: 'test level 1', rows: [
+            [0, 1, 2, 3, 4, 5, 6, 7]
+        ]};
+
+        levelCollection = {get: function (id) {
+            return level;
+        }};
+
+        spyOn(levelCollection, 'get').andCallThrough();
+
         GameCtrl = $controller('GameCtrl', {
             $scope: scope,
-            $routeParams: routeParams
+            $routeParams: routeParams,
+            levelCollection: levelCollection
         });
     }));
 
     it('should take the level id from the route params.', function () {
         expect(scope.levelId).toBe(routeParams.id);
+    });
+
+    it('should get a level from the levelCollection.', function () {
+        expect(scope.level).toBe(level);
+        expect(levelCollection.get.callCount).toBe(1);
     });
 });

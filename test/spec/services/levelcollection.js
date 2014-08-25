c18efb1d363cfb740fa7fe9512b9851f30fd0994
Simon Wendel <mail@simonwendel.se>
2014-08-25 11:22:50 +0200

    Extend the levelCollection spec with another level

--- test/spec/services/levelcollection.js
+++ test/spec/services/levelcollection.js
@@ -2,10 +2,10 @@
 
 describe('Factory: levelCollection', function () {
 
-    var levelCollection, levelDataMock;
+    var levelCollection;
     beforeEach(
         module('meanieBanApp', function ($provide) {
-            levelDataMock = [
+            var levelDataMock = [
                 {id: -2, width: 9, height: 9, name: 'test level 2', rows: [
                     [0, 7, 7, 1, 7, 7, 7, 7, 7],
                     [7, 7, 1, 7, 7, 1, 2, 1, 7],
@@ -16,6 +16,9 @@ describe('Factory: levelCollection', function () {
                     [7, 2, 2, 1, 7, 7, 1, 7, 7],
                     [7, 1, 1, 1, 7, 1, 7, 7],
                     [7, 7, 7, 7, 7, 1, 7]
+                ]},
+                {id: -3, width: 8, height: 1, name: 'test level 1', rows: [
+                    [0, 1, 2, 3, 4, 5, 6, 7]
                 ]}
             ];
 
@@ -30,12 +33,17 @@ describe('Factory: levelCollection', function () {
 
         it('should return all levels in the levelCollection.', function () {
             var levels = levelCollection.all();
-            expect(levels.length).toBe(1);
+            expect(levels.length).toBe(2);
 
             expect(levels[0].id).toBe(-2);
             expect(levels[0].name).toBe('test level 2');
             expect(levels[0].height).toBe(9);
             expect(levels[0].width).toBe(9);
+
+            expect(levels[1].id).toBe(-3);
+            expect(levels[1].name).toBe('test level 1');
+            expect(levels[1].height).toBe(1);
+            expect(levels[1].width).toBe(8);
         });
 
     });
@@ -44,14 +52,16 @@ describe('Factory: levelCollection', function () {
 
         it('should return all level names from the levelCollection.', function () {
             var names = levelCollection.names();
-            expect(names.length).toBe(1);
+            expect(names.length).toBe(2);
             expect(names[0].name).toBe('test level 2');
+            expect(names[1].name).toBe('test level 1');
         });
 
         it('should return all level ids from the levelCollection.', function () {
             var names = levelCollection.names();
-            expect(names.length).toBe(1);
+            expect(names.length).toBe(2);
             expect(names[0].id).toBe(-2);
+            expect(names[1].id).toBe(-3);
         });
 
     });

58539c900162bc665373cbac98d7214abbbac633
Simon Wendel <mail@simonwendel.se>
2014-09-06 22:48:15 +0200

    Sketch up flow in parse function in levelParser.js

--- importTool/levelParser.js
+++ importTool/levelParser.js
@@ -6,9 +6,7 @@ module.exports.getLevels = function (data) {
     var rows = data.split('\n');
     var currentCollectionName = rows[0];
     var current = { collectionName: currentCollectionName, data: '' };
-    for (var rowIndex = 0; rowIndex < rows.length; rowIndex++) {
-        var row = rows[rowIndex];
-
+    rows.forEach(function (row) {
         if (rowHasTiles(row)) {
             current.data = current.data.concat(row);
         } else if(row === '') {
@@ -17,21 +15,41 @@ module.exports.getLevels = function (data) {
         } else {
             currentCollectionName = row;
         }
-    }
+    });
 
     return levels.map(parse);
 };
 
 function parse(level, id) {
+    var voidsAdded = insertVoids(level.data);
+    var height = getHeight(voidsAdded);
+    var width = getWidth(voidsAdded);
+    var tiles = translate(voidsAdded);
     return {
         id: id,
         collection: level.collectionName,
-        width: 10,
-        height: 10,
-        rows: level.data
+        width: width,
+        height: height,
+        rows: tiles
     };
-};
+}
 
 function rowHasTiles(row) {
     return row[0] == ' ' || row[0] == '#';
+}
+
+function insertVoids(string) {
+    return string;
+}
+
+function translate(string) {
+    return string;
+}
+
+function getHeight(string) {
+    return 10;
+}
+
+function getWidth(string) {
+    return 10;
 }

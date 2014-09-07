f49b707c8bb99c1e693e6276ddb10d15fd8801fe
Simon Wendel <mail@simonwendel.se>
2014-09-07 14:10:02 +0200

    Reorganize the levelParser.js module

--- importTool/levelParser.js
+++ importTool/levelParser.js
@@ -23,54 +23,27 @@ module.exports.getLevels = function (data) {
     return levels.map(parse);
 };
 
+function rowHasTiles(row) {
+    return row[0] == ' ' || row[0] == '#';
+}
+
 function parse(level, id) {
     var voidsAdded = insertVoids(level.data);
     var height = getHeight(voidsAdded);
     var width = getWidth(voidsAdded);
     var tiles = translate(voidsAdded);
+    var rows = jsFormat(tiles);
     return {
         id: id,
         collection: level.collectionName,
         width: width,
         height: height,
-        rows: tiles
+        rows: rows
     };
-}
-
-function rowHasTiles(row) {
-    return row[0] == ' ' || row[0] == '#';
 }
 
 function insertVoids(string) {
     return string;
-}
-
-function translate(string) {
-    var translated = string
-        .replace(/V/g, '0')
-        .replace(/ /g, '1')
-        .replace(/\./g, '2')
-        .replace(/\*/g, '3')
-        .replace(/\$/g, '4')
-        .replace(/\+/g, '5')
-        .replace(/@/g, '6')
-        .replace(/#/g, '7');
-
-    return jsFormat(translated);
-}
-
-function jsFormat(rows) {
-    var rows = rows.split('\n');
-    rows.pop();
-    return rows.map(jsFormatLine);
-}
-
-function jsFormatLine(row, index, array) {
-    row = row.split('').join(',');
-    row = '[' + row + ']';
-    row = row.replace(/\n/g, '');
-
-    return row;
 }
 
 function getHeight(string) {
@@ -87,4 +60,30 @@ function getWidth(string) {
     });
 
     return maxIndex + 1;
+}
+
+function translate(string) {
+    return string
+        .replace(/V/g, '0')
+        .replace(/ /g, '1')
+        .replace(/\./g, '2')
+        .replace(/\*/g, '3')
+        .replace(/\$/g, '4')
+        .replace(/\+/g, '5')
+        .replace(/@/g, '6')
+        .replace(/#/g, '7');
+}
+
+function jsFormat(rows) {
+    var rows = rows.split('\n');
+    rows.pop();
+    return rows.map(jsFormatLine);
+}
+
+function jsFormatLine(row, index, array) {
+    row = row.split('').join(',');
+    row = '[' + row + ']';
+    row = row.replace(/\n/g, '');
+
+    return row;
 }

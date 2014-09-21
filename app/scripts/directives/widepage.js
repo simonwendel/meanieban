1a464c7a125efb51c15c45508d31e5bce19fad74
Simon Wendel <mail@simonwendel.se>
2014-09-21 20:46:46 +0200

    Re-include wide-page directive in code coverage analysis

--- app/scripts/directives/widepage.js
+++ app/scripts/directives/widepage.js
@@ -2,8 +2,6 @@
 
 angular.module('meanieBanApp')
     .directive('widePage', function () {
-        /* excluded from code coverage since this is only a template */
-        /* istanbul ignore next */
         return {
             templateUrl: 'views/directives/wide-page.html',
             restrict: 'E',

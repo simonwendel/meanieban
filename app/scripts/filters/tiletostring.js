074bd33f4d591c1f005860b989eba3784cc1b3ad
Simon Wendel <mail@simonwendel.se>
2014-09-21 23:10:52 +0200

    Make tileToString filter use the tileUtility to render tiles into strings

--- app/scripts/filters/tiletostring.js
+++ app/scripts/filters/tiletostring.js
@@ -1,27 +1,8 @@
 'use strict';
 
 angular.module('meanieBanApp')
-    .filter('tileToString', function () {
+    .filter('tileToString', function (tileUtility) {
         return function (input) {
-            switch (input) {
-                case 0:
-                    return 'void';
-                case 1:
-                    return 'floor';
-                case 2:
-                    return 'dock';
-                case 3:
-                    return 'box-docked';
-                case 4:
-                    return 'box';
-                case 5:
-                    return 'worker-docked';
-                case 6:
-                    return 'worker';
-                case 7:
-                    return 'wall';
-            }
-
-            throw new Error('Invalid input. Value must be an integer in [0,7].');
+            return tileUtility.tileToString(input);
         };
     });
--- test/spec/filters/tiletostring.js
+++ test/spec/filters/tiletostring.js
@@ -1,97 +1,32 @@
 'use strict';
 
-/*
-
- Standard text format of Sokoban levels (with my own additions):
-
- Level element 	    Character 	ASCII Code      My own value
- -------------      ---------   ----------      ------------
- Wall 	                # 	    0x23                7
- Player 	            @ 	    0x40                6
- Player on goal square 	+ 	    0x2b                5
- Box 	                $ 	    0x24                4
- Box on goal square 	* 	    0x2a                3
- Goal square 	        . 	    0x2e                2
- Floor 	             (Space) 	0x20                1
-
- My own: There is also the special case of 0 -> void.
-
- */
 describe('Filter: tileToString', function () {
 
-    // load the filter's module
     beforeEach(module('meanieBanApp'));
 
-    // initialize a new instance of the filter before each test
+    var utilityMock, returnValue;
+    beforeEach(
+        module('meanieBanApp', function ($provide) {
+            returnValue = {};
+            utilityMock = {
+                tileToString: function (input) {
+                    return returnValue;
+                }
+            };
+
+            spyOn(utilityMock, 'tileToString').andCallThrough();
+            $provide.value('tileUtility', utilityMock);
+        }));
+
     var tileToString;
     beforeEach(inject(function ($filter) {
         tileToString = $filter('tileToString');
     }));
 
-    it('should return "void" when given "0".', function () {
-        var tile = 0;
-        expect(tileToString(tile)).toBe('void');
+    it('should return use the tileUtility to get some value.', function () {
+        var tile = 15;
+        expect(tileToString(tile)).toBe(returnValue);
+        expect(utilityMock.tileToString.callCount).toBe(1);
     });
 
-    it('should return "floor" when given "1".', function () {
-        var tile = 1;
-        expect(tileToString(tile)).toBe('floor');
-    });
-
-    it('should return "dock" when given "2".', function () {
-        var tile = 2;
-        expect(tileToString(tile)).toBe('dock');
-    });
-
-    it('should return "box-docked" when given "3".', function () {
-        var tile = 3;
-        expect(tileToString(tile)).toBe('box-docked');
-    });
-
-    it('should return "box" when given "4".', function () {
-        var tile = 4;
-        expect(tileToString(tile)).toBe('box');
-    });
-
-    it('should return "worker-docked" when given "5".', function () {
-        var tile = 5;
-        expect(tileToString(tile)).toBe('worker-docked');
-    });
-
-    it('should return "worker" when given "6".', function () {
-        var tile = 6;
-        expect(tileToString(tile)).toBe('worker');
-    });
-
-    it('should return "wall" when given "7".', function () {
-        var tile = 7;
-        expect(tileToString(tile)).toBe('wall');
-    });
-
-    it('should throw an exception when given input < 0.', function () {
-        var tile = -1;
-        var functionCall = function () {
-            tileToString(tile);
-        };
-
-        expect(functionCall).toThrow('Invalid input. Value must be an integer in [0,7].');
-    });
-
-    it('should throw an exception when given input > 7.', function () {
-        var tile = 8;
-        var functionCall = function () {
-            tileToString(tile);
-        };
-
-        expect(functionCall).toThrow('Invalid input. Value must be an integer in [0,7].');
-    });
-
-    it('should throw an exception when given input which is not an integer.', function () {
-        var tile = 3.14;
-        var functionCall = function () {
-            tileToString(tile);
-        };
-
-        expect(functionCall).toThrow('Invalid input. Value must be an integer in [0,7].');
-    });
 });

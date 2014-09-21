3193e2202c6bbf3690b41baba4132220f24203af
Simon Wendel <mail@simonwendel.se>
2014-09-21 20:56:13 +0200

    Remove internal function name in GameCtrl

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('meanieBanApp')
-    .controller('GameCtrl', function GameCtrl($scope, $routeParams, levelCollection) {
+    .controller('GameCtrl', function ($scope, $routeParams, levelCollection) {
         var currentId = $routeParams.id;
         $scope.levelId = currentId;
         $scope.level = levelCollection.get(currentId);

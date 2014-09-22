1fb4901253fe22e165f2c666aa732fdfda854d32
Simon Wendel <mail@simonwendel.se>
2014-09-22 17:36:48 +0200

    Add workerLocation() to Grid

--- app/scripts/services/grid.js
+++ app/scripts/services/grid.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('meanieBanApp')
-    .factory('Grid', function () {
+    .factory('Grid', function (arrayUtility, tileUtility) {
 
         var Grid = function (gridArray) {
             if (!gridArray) {
@@ -11,8 +11,21 @@ angular.module('meanieBanApp')
             var grid = gridArray;
             this.getGrid = function () {
                 return grid;
-            }
+            };
+
+            this.workerLocation = function () {
+                return getWorkerLocation(grid);
+            };
         };
+
+        function getWorkerLocation(gridArray) {
+            var workerValue = tileUtility.stringToTile('worker');
+            var index = arrayUtility.get2dIndexOf(workerValue, gridArray);
+            return {
+                x: index[1],
+                y: index[0]
+            };
+        }
 
         return Grid;
 
--- test/spec/services/grid.js
+++ test/spec/services/grid.js
@@ -21,6 +21,11 @@ describe('Service: Grid', function () {
         expect(grid.getGrid()).toEqual(gridArray);
     });
 
+    it('should be able to get worker location in the Grid.', function () {
+        var grid = new Grid(gridArray);
+        expect(grid.workerLocation()).toEqual({x: 3, y: 1});
+    });
+
     it('should throw exception when grid is undefined to constructor.', function () {
         expect(function () {
 

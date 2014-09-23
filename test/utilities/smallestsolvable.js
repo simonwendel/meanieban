9e13967a6beebfb6a3d50b05c208a99eb3cb831e
Simon Wendel <mail@simonwendel.se>
2014-09-23 12:20:29 +0200

    Add comment as to the purpose of smallestSolvable

--- test/utilities/smallestsolvable.js
+++ test/utilities/smallestsolvable.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// this is only to be used in tests, so the same grid doesn't need to
+// be repeated over and over.
 angular.module('meanieBanApp')
     .factory('smallestSolvable', function (tileUtility) {
         return tileUtility.stringGridToTiles([

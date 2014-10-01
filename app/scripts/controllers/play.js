6b2ac635018b83b084961e2b3fe691142ae50869
Simon Wendel <mail@simonwendel.se>
2014-10-01 22:49:31 +0200

    Make PlayCtrl get level from collection once again

--- app/scripts/controllers/play.js
+++ app/scripts/controllers/play.js
@@ -3,5 +3,6 @@
 angular.module('meanieBanApp')
     .controller('PlayCtrl', function ($scope, $routeParams, LevelCollection) {
         var currentId = $routeParams.id;
-        var level = LevelCollection.get(currentId);
+        
+        $scope.level = LevelCollection.get(currentId);
     });
--- test/spec/controllers/play.js
+++ test/spec/controllers/play.js
@@ -4,13 +4,16 @@ describe('Controller: PlayCtrl', function () {
 
     beforeEach(module('meanieBanApp'));
 
-    var scope, routeParams, LevelCollection;
-
-    beforeEach(inject(function ($controller, $rootScope) {
+    var scope, routeParams, LevelCollection, level;
+    beforeEach(inject(function ($controller, $rootScope, smallestSolvable) {
         scope = $rootScope.$new();
         routeParams = { id: 6 };
 
-        LevelCollection = jasmine.createSpyObj('LevelCollection', ['get']);
+
+        level = smallestSolvable;
+
+        LevelCollection = { get: new Function() };
+        spyOn(LevelCollection, 'get').andReturn(level);
 
         $controller('PlayCtrl', {
             $scope: scope,
@@ -22,6 +25,7 @@ describe('Controller: PlayCtrl', function () {
     it('should get level data from the LevelCollection.', function () {
         expect(LevelCollection.get.callCount).toBe(1);
         expect(LevelCollection.get).toHaveBeenCalledWith(routeParams.id);
+        expect(scope.level).toBe(level);
     });
 
 });

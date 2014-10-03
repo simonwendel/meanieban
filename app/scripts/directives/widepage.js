205479f348c1eaea26fd0aaee3efa4ce2e77e08f
Simon Wendel <mail@simonwendel.se>
2014-10-03 18:44:22 +0200

    Tidy up widepage.js

--- app/scripts/directives/widepage.js
+++ app/scripts/directives/widepage.js
@@ -2,9 +2,11 @@
 
 angular.module('meanieBanApp')
     .directive('widePage', function () {
+
         return {
             templateUrl: 'views/directives/wide-page.html',
             restrict: 'E',
             transclude: true
         };
+
     });

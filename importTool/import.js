f23d2d31ef5250b7cf9b25a4bf9c7a4d501b123e
Simon Wendel <mail@simonwendel.se>
2014-10-03 18:02:58 +0200

    Expand documentation comment on the importer node module

--- importTool/import.js
+++ importTool/import.js
@@ -1,5 +1,7 @@
-/*
+/**
  * Node module to create the levelData value for the ng-app.
+ *
+ * run by: nodejs import.js <level data filename>
  */
 var fs = require('fs');
 var parser = require('./textParser');

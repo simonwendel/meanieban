b7fb72028b5b72072694d2d485db772c4e91d0b3
Simon Wendel <mail@simonwendel.se>
2014-10-03 18:43:43 +0200

    Tidy up narrowpage.js

--- app/scripts/directives/narrowpage.js
+++ app/scripts/directives/narrowpage.js
@@ -2,9 +2,11 @@
 
 angular.module('meanieBanApp')
     .directive('narrowPage', function () {
+
         return {
             templateUrl: 'views/directives/narrow-page.html',
             restrict: 'E',
             transclude: true
         };
+
     });

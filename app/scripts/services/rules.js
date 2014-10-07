2ec850247486376e2a8cd9107063de4ffb8d63dd
Simon Wendel <mail@simonwendel.se>
2014-10-07 18:46:25 +0200

    Add Rules service

---
+++ app/scripts/services/rules.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/*
+ Standard text format of Sokoban levels (with my own additions):
+
+ Level element 	    Character 	ASCII Code      My own value
+ -------------      ---------   ----------      ------------
+ Wall 	                # 	    0x23                7
+ Player 	            @ 	    0x40                6
+ Player on goal square 	+ 	    0x2b                5
+ Box 	                $ 	    0x24                4
+ Box on goal square 	* 	    0x2a                3
+ Goal square 	        . 	    0x2e                2
+ Floor 	             (Space) 	0x20                1
+
+ My own: There is also the special case of 0 -> void.
+ */
+angular.module('meanieBanApp')
+    .service('Rules', function Rules() {
+    });
---
+++ test/spec/services/rules.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('Service: Rules', function () {
+
+    beforeEach(module('meanieBanApp'));
+
+    var Rules;
+    beforeEach(inject(function (_Rules_) {
+        Rules = _Rules_;
+    }));
+
+});

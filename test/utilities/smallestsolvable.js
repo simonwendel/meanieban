74d4b506ce7e80532c01bd493bde090a93aea1b8
Simon Wendel <mail@simonwendel.se>
2014-10-08 19:01:38 +0200

    Append the comment in the smallestSolvable utility

--- test/utilities/smallestsolvable.js
+++ test/utilities/smallestsolvable.js
@@ -1,7 +1,8 @@
 'use strict';
 
 // this is only to be used in tests, so the same grid doesn't need to
-// be repeated over and over.
+// be repeated over and over. it is the smallest solvable level that
+// can be produced in Sokoban.
 angular.module('meanieBanApp')
     .factory('smallestSolvable', function (tileUtility) {
         return tileUtility.stringGridToTiles([

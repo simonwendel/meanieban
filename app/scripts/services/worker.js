0d22c7d1291d64520a7cfd65981f3a616351e441
Simon Wendel <mail@simonwendel.se>
2014-10-09 15:53:58 +0200

    Add function to update location in Worker

--- app/scripts/services/worker.js
+++ app/scripts/services/worker.js
@@ -4,5 +4,10 @@ angular.module('meanieBanApp')
     .factory('Worker', function () {
         return function (x, y) {
             this.location = {x: x, y: y};
+
+            this.update = function (delta) {
+                this.location.x += delta.x;
+                this.location.y += delta.y;
+            };
         };
     });
--- test/spec/services/worker.js
+++ test/spec/services/worker.js
@@ -14,4 +14,10 @@ describe('Service: Worker', function () {
         expect(worker.location).toEqual({ x: 3, y: 5});
     });
 
+    it('should have an update function updating the location by deltas.', function () {
+        var worker = new Worker(3, 5);
+        worker.update({x: 1, y: -1});
+        expect(worker.location).toEqual({ x: 4, y: 4});
+    });
+
 });

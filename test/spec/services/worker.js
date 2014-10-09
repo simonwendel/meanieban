46eede4d68ee9169bb85eba169dca27d16bff9d4
Simon Wendel <mail@simonwendel.se>
2014-10-09 15:46:08 +0200

    Add Worker constructor to the application

---
+++ test/spec/services/worker.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('Service: Worker', function () {
+
+    beforeEach(module('meanieBanApp'));
+
+    var Worker;
+    beforeEach(inject(function (_Worker_) {
+        Worker = _Worker_;
+    }));
+
+});

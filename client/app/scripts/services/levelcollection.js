1f02bc305a76756230733272ed67ef1efbd31922
Simon Wendel <mail@simonwendel.se>
2014-10-13 21:33:01 +0200

    Fix JHint errors in levelcollection.js
    Some =='s are replaced with ==='s and unfortunately named output
    variable is renamed.

--- client/app/scripts/services/levelcollection.js
+++ client/app/scripts/services/levelcollection.js
@@ -4,25 +4,25 @@ angular.module('meanieBanApp')
     .service('LevelCollection', function (levelData, arrayUtility, tileUtility) {
         // this is O(n^2) or similar
         function collections() {
-            var collections = [];
+            var output = [];
             var names = arrayUtility.getUniqueValuesOf('collection', levelData);
             names.forEach(function (name) {
-                collections.push({
+                output.push({
                     name: name,
                     levels: levelData.map(function (level) {
-                        if (level.collection == name) {
+                        if (level.collection === name) {
                             return { id: level.id };
                         }
                     })
                 });
             });
 
-            return collections;
+            return output;
         }
 
         function get(id) {
             var found = levelData.filter(function (level) {
-                return level.id == id;
+                return level.id === id;
             })[0];
 
             if (found) {

75d24477f4277357596b6ede1193cf50b1acc6f5
Simon Wendel <mail@simonwendel.se>
2014-10-13 21:37:12 +0200

    Fix if block curly braces missing in arrayutility.js

--- client/app/scripts/services/arrayutility.js
+++ client/app/scripts/services/arrayutility.js
@@ -9,7 +9,10 @@ angular.module('meanieBanApp')
                 length = inArray.length;
 
             for (var i = 0; i < length; i++) {
-                if (flags[inArray[i][property]]) continue;
+                if (flags[inArray[i][property]]) {
+                    continue;
+                }
+
                 flags[inArray[i][property]] = true;
                 output.push(inArray[i][property]);
             }

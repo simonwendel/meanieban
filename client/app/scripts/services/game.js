ac042d5f01014030e84747b4291cf70909dcb8b2
Simon Wendel <mail@simonwendel.se>
2014-10-16 22:36:18 +0200

    Re-order the Game ctor function definition

--- client/app/scripts/services/game.js
+++ client/app/scripts/services/game.js
@@ -8,28 +8,29 @@ angular.module('meanieBanApp')
                 throw new Error('Parameter level to constructor function cannot be undefined.');
             }
 
-            if(!(level instanceof Level)) {
+            if (!(level instanceof Level)) {
                 throw new Error('Parameter level to constructor function must be an instance of Level.');
             }
 
-            this.move = function () {
-
-            };
-
             var numberOfMoves = 0;
-            this.moves = function () {
-                return numberOfMoves;
-            };
 
-            this.grid = function () {
+            function move() {
+
+            }
+
+            function moves() {
+                return numberOfMoves;
+            }
+
+            function grid() {
                 return level.grid();
-            };
+            }
 
             // O(n) over number of cells, worst
             // case if game is finished
-            this.isFinished = function () {
-                for (var r = 0; r < this.grid().length; r++) {
-                    var row = this.grid()[r];
+            function isFinished() {
+                for (var r = 0; r < grid().length; r++) {
+                    var row = grid()[r];
                     for (var c = 0; c < row.length; c++) {
                         var cell = row[c];
                         if (Rules.isOpenDock(cell)) {
@@ -39,7 +40,17 @@ angular.module('meanieBanApp')
                 }
 
                 return true;
-            };
+            }
+
+            // expose //
+
+            this.move = move;
+
+            this.moves = moves;
+
+            this.grid = grid;
+
+            this.isFinished = isFinished;
         };
 
     });

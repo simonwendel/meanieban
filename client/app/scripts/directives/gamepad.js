86350be81b8073f186a88ebbec8f826b0f6701a8
Simon Wendel <mail@simonwendel.se>
2014-10-26 18:16:57 +0100

    Make game-pad move classes down to internal table

--- client/app/scripts/directives/gamepad.js
+++ client/app/scripts/directives/gamepad.js
@@ -4,6 +4,11 @@ angular.module('meanieBanApp')
     .directive('gamePad', function () {
         return {
             templateUrl: 'views/directives/game-pad.html',
-            restrict: 'E'
+            restrict: 'E',
+            link: function (scope, element) {
+                var classes = element.attr('class');
+                element.removeAttr('class');
+                element.children('table').attr('class', classes);
+            }
         };
     });
--- client/test/spec/directives/gamepad.js
+++ client/test/spec/directives/gamepad.js
@@ -4,16 +4,26 @@ describe('Directive: gamePad', function () {
 
     beforeEach(module('meanieBanApp'));
 
+    beforeEach(module('views/directives/game-pad.html'));
+
     var element, scope;
     beforeEach(inject(function ($rootScope, $compile) {
         scope = $rootScope.$new();
-        element = angular.element('<game-pad></game-pad>');
+        element = angular.element('<game-pad class="some classes"></game-pad>');
         element = $compile(element)(scope);
+        scope.$digest();
+
         scope = element.isolateScope();
     }));
 
     it('should be defined.', function () {
         expect(element.html()).toBeDefined();
+    });
+
+    it('should move classes down to the table element.', function () {
+        var classes = element.find('table').attr('class');
+        expect(classes).toContain('some');
+        expect(classes).toContain('classes');
     });
 
 });

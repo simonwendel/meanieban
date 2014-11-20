08a37da76cee65dc37ba44076c18bfd1eb0931ad
Simon Wendel <mail@simonwendel.se>
2014-11-20 17:19:28 +0100

    Add force option to grunt clean task
    The clean task will not remove directories outside of working dir, so
    a --force option is specified to make it do so anyway.

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -133,6 +133,9 @@ module.exports = function (grunt) {
         // Empties folders to start fresh
         clean: {
             dist: {
+                options: {
+                    force: true
+                },
                 files: [
                     {
                         dot: true,

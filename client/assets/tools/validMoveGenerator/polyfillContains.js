16ad49d8608d6c48448adf767b01261a728ee940
Simon Wendel <mail@simonwendel.se>
2014-11-24 20:04:11 +0100

    Add JSHint ignore and explanation to polyfill

--- client/assets/tools/validMoveGenerator/polyfillContains.js
+++ client/assets/tools/validMoveGenerator/polyfillContains.js
@@ -1,7 +1,14 @@
 'use strict';
 
+/**
+ * Polyfills the contains function onto String, which
+ * returns true iff a string is a substring of another.
+ *
+ * JSHint is told to ignore this, since this is a place
+ * we really want to extend the native prototype.
+ */
 if (!String.prototype.contains) {
-    String.prototype.contains = function() {
+    String.prototype.contains = function() { // jshint ignore:line
         return String.prototype.indexOf.apply(this, arguments) !== -1;
     };
 }

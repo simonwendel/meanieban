8a7addb6cc861e2c722de01dfa621f2d8563d0d5
Simon Wendel <mail@simonwendel.se>
2014-12-01 21:54:15 +0100

    Reformat collection-selector to new standard

--- client/app/scripts/directives/collectionselector.js
+++ client/app/scripts/directives/collectionselector.js
@@ -1,26 +1,40 @@
-'use strict';
+;(function() {
+    'use strict';
 
-angular.module('meanieBanApp')
-    .directive('collectionSelector', ['LevelCollection', function (LevelCollection) {
+    angular
+        .module('meanieBanApp')
+        .directive('swCollectionSelector', collectionSelector);
+
+    function collectionSelector() {
         return {
             templateUrl: 'views/directives/collection-selector.html',
             restrict: 'E',
             scope: {
                 selectedCallback: '='
             },
-            controller: ['$scope', function ($scope) {
-                $scope.renderLabel = function (col) {
-                    var levelCount = ' level';
-                    if(col.levels.length > 1) {
-                        levelCount += 's';
-                    }
-
-                    return col.name + ' - ' + col.levels.length + levelCount;
-                };
-
-                $scope.collections = LevelCollection.collections();
-                $scope.selected = $scope.collections[0].name;
-                $scope.selectedCallback($scope.selected);
-            }]
+            controller: CollectionSelectorCtrl,
+            controllerAs: 'vm'
         };
-    }]);
+    }
+
+    /** @ngInject */
+    function CollectionSelectorCtrl($scope, LevelCollection) {
+        var vm = this;
+
+        vm.renderLabel = renderLabel;
+        vm.collections = LevelCollection.collections();
+        vm.selected = vm.collections[0].name;
+        vm.selectedCallback = $scope.selectedCallback;
+
+        vm.selectedCallback(vm.selected);
+
+        function renderLabel(col) {
+            var levelCount = ' level';
+            if (col.levels.length > 1) {
+                levelCount += 's';
+            }
+
+            return col.name + ' - ' + col.levels.length + levelCount;
+        }
+    }
+})();
--- client/test/spec/directives/collectionselector.js
+++ client/test/spec/directives/collectionselector.js
@@ -1,65 +1,71 @@
-'use strict';
+;(function() {
+    'use strict';
 
-describe('Directive: collectionSelector', function () {
-
-    var collections,
+    var scope,
+        collections,
         mockLevelCollection,
-        scope,
         pageScope;
 
-    beforeEach(module('meanieBanApp', function ($provide) {
+    describe('Directive: collectionSelector', function() {
+
+        beforeEach(module('views/directives/collection-selector.html'));
+
+        beforeEach(module('meanieBanApp', provideSetup));
+
+        beforeEach(inject(fixtureSetup));
+
+        it('should get all collections and attach them to scope.', function() {
+            expect(scope.vm.collections).toBe(collections);
+            expect(mockLevelCollection.collections.callCount).toBe(1);
+        });
+
+        it('should attach the name of the first level returned to the selected property.', function() {
+            expect(scope.vm.selected).toBe(collections[0].name);
+        });
+
+        it('should call the selected-callback with the scope.selected on init.', function() {
+            expect(pageScope.selected.callCount).toBe(1);
+            expect(pageScope.selected).toHaveBeenCalledWith(scope.vm.selected);
+        });
+
+        it('should have a renderLabel function that renders the option label with single level.', function() {
+            expect(scope.vm.renderLabel(collections[1])).toBe('Mini - 1 level');
+        });
+
+        it('should have a renderLabel function that renders the option label with multiple levels.', function() {
+            expect(scope.vm.renderLabel(collections[0])).toBe('Sasquatch - 3 levels');
+        });
+    });
+
+    function provideSetup($provide) {
         collections = [
             {name: 'Sasquatch', levels: [{id: 1}, {id: 2}, {id: 3}]},
             {name: 'Mini', levels: [{id: 1}]}
         ];
 
         mockLevelCollection = {
-            collections: function () {
+            collections: function() {
                 return collections;
             }
         };
 
         $provide.value('LevelCollection', mockLevelCollection);
-    }));
+    }
 
-    beforeEach(module('views/directives/collection-selector.html'));
-
-    beforeEach(inject(function ($rootScope, $compile) {
+    function fixtureSetup($rootScope, $compile) {
         var element = angular.element(
-            '<collection-selector selected-callback="selected"></collection-selector>');
+            '<sw-collection-selector selected-callback="selected"></sw-collection-selector>');
 
         spyOn(mockLevelCollection, 'collections').andCallThrough();
 
         pageScope = $rootScope.$new();
-        pageScope.selected = function () {};
+        pageScope.selected = function() {
+        };
         spyOn(pageScope, 'selected');
 
         element = $compile(element)(pageScope);
 
         pageScope.$digest();
         scope = element.isolateScope();
-    }));
-
-    it('should get all collections and attach them to scope.', function () {
-        expect(scope.collections).toBe(collections);
-        expect(mockLevelCollection.collections.callCount).toBe(1);
-    });
-
-    it('should attach the name of the first level returned to the selected property.', function () {
-        expect(scope.selected).toBe(collections[0].name);
-    });
-
-    it('should call the selected-callback with the scope.selected on init.', function () {
-        expect(pageScope.selected.callCount).toBe(1);
-        expect(pageScope.selected).toHaveBeenCalledWith(scope.selected);
-    });
-
-    it('should have a renderLabel function that renders the option label with single level.', function () {
-        expect(scope.renderLabel(collections[1])).toBe('Mini - 1 level');
-    });
-
-    it('should have a renderLabel function that renders the option label with multiple levels.', function () {
-        expect(scope.renderLabel(collections[0])).toBe('Sasquatch - 3 levels');
-    });
-
-});
+    }
+})();

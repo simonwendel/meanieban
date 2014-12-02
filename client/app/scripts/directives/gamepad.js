97321b5ca0e0c157c33dfb106cedc6ffa24b9ac0
Simon Wendel <mail@simonwendel.se>
2014-12-02 15:30:49 +0100

    Re-format game pad according to new standard

--- client/app/scripts/directives/gamepad.js
+++ client/app/scripts/directives/gamepad.js
@@ -1,23 +1,35 @@
-'use strict';
+;(function() {
+    'use strict';
 
-angular.module('meanieBanApp')
-    .directive('gamePad', function () {
+    angular.module('meanieBanApp')
+        .directive('swGamePad', gamePad);
 
+    function gamePad() {
         return {
             templateUrl: 'views/directives/game-pad.html',
             restrict: 'E',
             scope: {
                 move: '='
             },
-            link: function (scope, element) {
-                if (!(scope.move instanceof Function)) {
-                    throw new Error('Passed-in move is not a Function.');
-                }
-
-                var classes = element.attr('class');
-                element.removeAttr('class');
-                element.children('table').attr('class', classes);
-            }
+            link: link,
+            controller: GamePadCtrl,
+            controllerAs: 'vm'
         };
+    }
 
-    });
+    function link(scope, element) {
+        var classes = element.attr('class');
+        element.removeAttr('class');
+        element.children('table').attr('class', classes);
+    }
+
+    /** @ngInject */
+    function GamePadCtrl($scope) {
+        var vm = this;
+        if (!($scope.move instanceof Function)) {
+            throw new Error('Passed-in move is not a Function.');
+        }
+
+        vm.move = $scope.move;
+    }
+})();
--- client/test/spec/directives/gamepad.js
+++ client/test/spec/directives/gamepad.js
@@ -1,47 +1,55 @@
-'use strict';
+;(function() {
+    'use strict';
 
-describe('Directive: gamePad', function () {
+    var emptyFunction = function() {},
+        element,
+        scope,
+        pageScope;
 
-    beforeEach(module('meanieBanApp'));
+    describe('Directive: gamePad', function() {
 
-    beforeEach(module('views/directives/game-pad.html'));
+        beforeEach(module('views/directives/game-pad.html'));
 
-    beforeEach(function () {
-        buildUp(function () {});
+        beforeEach(module('meanieBanApp'));
+
+        beforeEach(fixtureSetup);
+
+        it('should be defined.', function() {
+            expect(element.html()).toBeDefined();
+        });
+
+        it('should move classes down to the table element.', function() {
+            var classes = element.find('table').attr('class');
+            expect(classes).toContain('some');
+            expect(classes).toContain('classes');
+        });
+
+        it('should have a handler function attached to scope.', function() {
+            expect(scope.vm.move instanceof Function).toBeTruthy();
+        });
+
+        it('should throw exception if the attached move is not a function.', function() {
+            expect(function() {
+                fixtureSetup({});
+            }).toThrow('Passed-in move is not a Function.');
+        });
     });
 
-    var element, scope, pageScope;
-    function buildUp(move) {
-        inject(function ($rootScope, $compile) {
+    function fixtureSetup(move) {
+        inject(function($rootScope, $compile) {
             pageScope = $rootScope.$new();
-            pageScope.move = move;
 
-            element = angular.element('<game-pad move="move" class="some classes"></game-pad>');
+            if (move) {
+                pageScope.move = move;
+            } else {
+                pageScope.move = emptyFunction;
+            }
+
+            element = angular.element('<sw-game-pad move="move" class="some classes"></sw-game-pad>');
             element = $compile(element)(pageScope);
             pageScope.$digest();
 
             scope = element.isolateScope();
         });
     }
-
-    it('should be defined.', function () {
-        expect(element.html()).toBeDefined();
-    });
-
-    it('should move classes down to the table element.', function () {
-        var classes = element.find('table').attr('class');
-        expect(classes).toContain('some');
-        expect(classes).toContain('classes');
-    });
-
-    it('should have a handler function attached to scope.', function () {
-        expect(scope.move instanceof Function).toBeTruthy();
-    });
-
-    it('should throw exception if the attached move is not a function.', function () {
-        expect(function () {
-            buildUp({});
-        }).toThrow('Passed-in move is not a Function.');
-    });
-
-});
+})();

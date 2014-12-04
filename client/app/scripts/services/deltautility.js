c826b4f8c26e67b634c507a57d528ecf415302d2
Simon Wendel <mail@simonwendel.se>
2014-12-04 16:51:26 +0100

    Reformat deltaUtility
    The exact placement of directionsDeltaMap is not determined completely
    by this commit.

--- client/app/scripts/services/deltautility.js
+++ client/app/scripts/services/deltautility.js
@@ -1,36 +1,39 @@
-'use strict';
+;(function() {
+    'use strict';
 
-angular.module('meanieBanApp')
-    .factory('deltaUtility', function () {
+    angular.module('meanieBanApp')
+        .factory('deltaUtility', deltaUtility);
 
-        var directionsDeltaMap = {
-            'up': {
-                dx: 0, dy: -1
-            },
-            'down': {
-                dx: 0, dy: 1
-            },
-            'left': {
-                dx: -1, dy: 0
-            },
-            'right': {
-                dx: 1, dy: 0
-            }};
-
-        function compute(direction) {
-            var delta = directionsDeltaMap[direction];
-            if (!delta) {
-                throw new Error('Direction must be "up", "down", "left" or "right".');
-            }
-
-            return {
-                x: delta.dx,
-                y: delta.dy
-            };
-        }
-
+    function deltaUtility() {
         return {
             compute: compute
         };
+    }
 
-    });
+    var directionsDeltaMap = {
+        up: {
+            dx: 0, dy: -1
+        },
+        down: {
+            dx: 0, dy: 1
+        },
+        left: {
+            dx: -1, dy: 0
+        },
+        right: {
+            dx: 1, dy: 0
+        }
+    };
+
+    function compute(direction) {
+        var delta = directionsDeltaMap[direction];
+        if (!delta) {
+            throw new Error('Direction must be "up", "down", "left" or "right".');
+        }
+
+        return {
+            x: delta.dx,
+            y: delta.dy
+        };
+    }
+})();
--- client/test/spec/services/deltautility.js
+++ client/test/spec/services/deltautility.js
@@ -1,39 +1,43 @@
-'use strict';
-
-describe('Service: deltaUtility', function () {
-
-    beforeEach(module('meanieBanApp'));
+;(function() {
+    'use strict';
 
     var deltaUtility;
-    beforeEach(inject(function (_deltaUtility_) {
+
+    describe('Service: deltaUtility', function() {
+
+        beforeEach(module('meanieBanApp'));
+
+        beforeEach(inject(fixtureSetup));
+
+        it('should throw exception if passed other than direction.', function() {
+            expect(function() {
+
+                deltaUtility.compute('east');
+
+            }).toThrow('Direction must be "up", "down", "left" or "right".');
+
+            expect(function() {
+
+                deltaUtility.compute({});
+
+            }).toThrow('Direction must be "up", "down", "left" or "right".');
+
+            expect(function() {
+
+                deltaUtility.compute(undefined);
+
+            }).toThrow('Direction must be "up", "down", "left" or "right".');
+        });
+
+        it('should compute deltas according to direction.', function() {
+            expect(deltaUtility.compute('up')).toEqual({x: 0, y: -1});
+            expect(deltaUtility.compute('down')).toEqual({x: 0, y: 1});
+            expect(deltaUtility.compute('left')).toEqual({x: -1, y: 0});
+            expect(deltaUtility.compute('right')).toEqual({x: 1, y: 0});
+        });
+    });
+
+    function fixtureSetup(_deltaUtility_) {
         deltaUtility = _deltaUtility_;
-    }));
-
-    it('should throw exception if passed other than direction.', function () {
-        expect(function () {
-
-            deltaUtility.compute('east');
-
-        }).toThrow('Direction must be "up", "down", "left" or "right".');
-
-        expect(function () {
-
-            deltaUtility.compute({});
-
-        }).toThrow('Direction must be "up", "down", "left" or "right".');
-
-        expect(function () {
-
-            deltaUtility.compute(undefined);
-
-        }).toThrow('Direction must be "up", "down", "left" or "right".');
-    });
-
-    it('should compute deltas according to direction.', function () {
-        expect(deltaUtility.compute('up')).toEqual({x: 0, y: -1});
-        expect(deltaUtility.compute('down')).toEqual({x: 0, y: 1});
-        expect(deltaUtility.compute('left')).toEqual({x: -1, y: 0});
-        expect(deltaUtility.compute('right')).toEqual({x: 1, y: 0});
-    });
-
-});
+    }
+})();

a2ffb849994e1afc39a50f73ada351a1ccbe9e66
Simon Wendel <mail@simonwendel.se>
2014-12-04 11:56:40 +0100

    Reformat and update sokoban-grid

--- client/app/scripts/directives/sokobangrid.js
+++ client/app/scripts/directives/sokobangrid.js
@@ -1,10 +1,12 @@
-'use strict';
+;(function() {
+    'use strict';
 
-angular.module('meanieBanApp')
-    .directive('sokobanGrid', function () {
+    angular.module('meanieBanApp')
+        .directive('swSokobanGrid', sokobanGrid);
 
+    function sokobanGrid() {
         return {
-            templateUrl: 'views/directives/sokoban-grid.html',
+            templateUrl: 'views/directives/sw-sokoban-grid.html',
             restrict: 'AE',
             replace: false,
             scope: {
@@ -12,19 +14,32 @@ angular.module('meanieBanApp')
                 keydown: '=',
                 skin: '='
             },
-            link: function (scope, element) {
-                if(typeof scope.keydown !== 'function') {
-                    throw new Error('Scope variable keydown must be a function.');
-                }
-
-                var classes = element.attr('class');
-                element.removeAttr('class');
-                element.children('table').attr('class', classes);
-
-                $(element).closest('body').on('keydown', function (event) {
-                    scope.keydown(event);
-                });
-            }
+            link: link,
+            controller: SokobanGridCtrl,
+            controllerAs: 'vm'
         };
 
-    });
+        function link(scope, element) {
+            var classes = element.attr('class');
+            element.removeAttr('class');
+            element.children('table').attr('class', classes);
+        }
+    }
+
+    /** @ngInject */
+    function SokobanGridCtrl($scope, $element) {
+        var vm = this;
+
+        vm.keydown = $scope.keydown;
+        vm.skin = $scope.skin;
+        vm.grid = $scope.grid;
+
+        if (typeof vm.keydown !== 'function') {
+            throw new Error('Scope variable keydown must be a function.');
+        }
+
+        $($element).closest('body').on('keydown', function(event) {
+            vm.keydown(event);
+        });
+    }
+})();
--- client/test/spec/directives/sokobangrid.js
+++ client/test/spec/directives/sokobangrid.js
@@ -1,10 +1,55 @@
-'use strict';
+(function() {
+    'use strict';
 
-describe('Directive: sokobanGrid', function () {
+    var element,
+        scope,
+        pageScope,
+        smallestSolvable;
 
-    beforeEach(module('meanieBanApp'));
+    describe('Directive: sokobanGrid', function() {
 
-    beforeEach(module('views/directives/sokoban-grid.html'));
+        beforeEach(module('views/directives/sw-sokoban-grid.html'));
+
+        beforeEach(module('meanieBanApp'));
+
+        beforeEach(inject(fixtureSetup));
+
+        it('should get settings from the grid attribute and build html.', function() {
+            expect(scope.vm.grid).toBe(pageScope.level.rows);
+            expect(element.html()).toBeDefined();
+            expect(element.html()).not.toBe('');
+        });
+
+        it('should attach skin attribute to directive scope.', function() {
+            expect(scope.vm.skin).toBe(pageScope.skin);
+        });
+
+        it('should move classes down to the table element.', function() {
+            var classes = element.find('table').attr('class');
+            expect(classes).toContain('some');
+            expect(classes).toContain('classes');
+        });
+
+        it('should attach key-down handler to the directive scope.', function() {
+            expect(scope.vm.keydown).toBe(pageScope.keydown);
+        });
+
+        it('should require key-down handler to be a function.', inject(function($rootScope, $compile) {
+            expect(function() {
+
+                buildDirective($rootScope, $compile, {});
+
+            }).toThrow('Scope variable keydown must be a function.');
+        }));
+    });
+
+    function fixtureSetup($rootScope, $compile, _smallestSolvable_) {
+        var keydownHandler = function() {
+        };
+
+        smallestSolvable = _smallestSolvable_;
+        buildDirective($rootScope, $compile, keydownHandler);
+    }
 
     function buildDirective($rootScope, $compile, keydownHandler) {
         pageScope = $rootScope.$new();
@@ -20,52 +65,14 @@ describe('Directive: sokobanGrid', function () {
         pageScope.skin = 'some skin';
 
         element = angular.element(
-                '<body>' +
-                '<sokoban-grid keydown="keydown" grid="level.rows" skin="skin" class="some classes"></sokoban-grid>' +
-                '</body>');
+            '<body>' +
+            '<sw-sokoban-grid keydown="keydown" grid="level.rows" skin="skin" class="some classes"></sw-sokoban-grid>' +
+            '</body>');
 
         element = $compile(element)(pageScope);
 
         pageScope.$digest();
 
-        directiveScope = element.isolateScope();
+        scope = element.isolateScope();
     }
-
-    var element, directiveScope, pageScope, smallestSolvable;
-    beforeEach(inject(function ($rootScope, $compile, _smallestSolvable_) {
-        var keydownHandler = function () {
-        };
-
-        smallestSolvable = _smallestSolvable_;
-        buildDirective($rootScope, $compile, keydownHandler);
-    }));
-
-    it('should get settings from the grid attribute and build html.', function () {
-        expect(directiveScope.grid).toBe(pageScope.level.rows);
-        expect(element.html()).toBeDefined();
-        expect(element.html()).not.toBe('');
-    });
-
-    it('should attach skin attribute to directive scope.', function () {
-        expect(directiveScope.skin).toBe(pageScope.skin);
-    });
-
-    it('should move classes down to the table element.', function () {
-        var classes = element.find('table').attr('class');
-        expect(classes).toContain('some');
-        expect(classes).toContain('classes');
-    });
-
-    it('should attach key-down handler to the directive scope.', function () {
-        expect(directiveScope.keydown).toBe(pageScope.keydown);
-    });
-
-    it('should require key-down handler to be a function.', inject(function ($rootScope, $compile) {
-        expect(function () {
-
-            buildDirective($rootScope, $compile, {});
-
-        }).toThrow('Scope variable keydown must be a function.');
-    }));
-
-});
+})();

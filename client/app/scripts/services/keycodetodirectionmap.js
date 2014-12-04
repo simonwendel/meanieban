0cca44b6fd149d8c164c2002fb62fce79a5e223d
Simon Wendel <mail@simonwendel.se>
2014-12-04 16:58:20 +0100

    Reformat the keyCodeToDirectionMap

--- client/app/scripts/services/keycodetodirectionmap.js
+++ client/app/scripts/services/keycodetodirectionmap.js
@@ -1,9 +1,17 @@
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('meanieBanApp')
-    .value('keyCodeToDirectionMap', {
+    /**
+     * These correspond to the event.keyCode codes produced by
+     * the four arrow keys.
+     **/
+    var keyCodeToDirectionMap = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
-    });
+    };
+
+    angular.module('meanieBanApp')
+        .value('keyCodeToDirectionMap', keyCodeToDirectionMap);
+})();
--- client/test/spec/services/keycodetodirectionmap.js
+++ client/test/spec/services/keycodetodirectionmap.js
@@ -1,25 +1,28 @@
-'use strict';
-
-describe('Service: keyCodeToDirectionMap', function () {
-
-    beforeEach(module('meanieBanApp'));
+;(function() {
+    'use strict';
 
     var keyCodeToDirectionMap;
 
-    beforeEach(inject(function (_keyCodeToDirectionMap_) {
+    describe('Service: keyCodeToDirectionMap', function() {
+
+        beforeEach(module('meanieBanApp'));
+
+        beforeEach(inject(fixtureSetup));
+
+        it('should have exactly four mappings.', function() {
+            var numberOfMappings = Object.keys(keyCodeToDirectionMap).length;
+            expect(numberOfMappings).toBe(4);
+        });
+
+        it('should contain mapping for up, down, left and right keys.', function() {
+            expect(keyCodeToDirectionMap[37]).toBe('left');
+            expect(keyCodeToDirectionMap[38]).toBe('up');
+            expect(keyCodeToDirectionMap[39]).toBe('right');
+            expect(keyCodeToDirectionMap[40]).toBe('down');
+        });
+    });
+
+    function fixtureSetup(_keyCodeToDirectionMap_) {
         keyCodeToDirectionMap = _keyCodeToDirectionMap_;
-    }));
-
-    it('should have exactly four mappings.', function () {
-        var numberOfMappings = Object.keys(keyCodeToDirectionMap).length;
-        expect(numberOfMappings).toBe(4);
-    });
-
-    it('should contain mapping for up, down, left and right keys.', function () {
-        expect(keyCodeToDirectionMap[37]).toBe('left');
-        expect(keyCodeToDirectionMap[38]).toBe('up');
-        expect(keyCodeToDirectionMap[39]).toBe('right');
-        expect(keyCodeToDirectionMap[40]).toBe('down');
-    });
-
-});
+    }
+})();

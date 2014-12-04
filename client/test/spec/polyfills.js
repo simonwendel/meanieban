e91beb66865565063adebc6ad263488f8f476dc1
Simon Wendel <mail@simonwendel.se>
2014-12-04 12:47:15 +0100

    Reformat polyfills.js with test file

--- client/test/spec/polyfills.js
+++ client/test/spec/polyfills.js
@@ -1,11 +1,12 @@
-'use strict';
+;(function() {
+    'use strict';
 
-describe('Polyfills from ECMAScript 6', function () {
+    describe('Polyfills (polyfills.js)', function() {
 
-    it('should polyfill a working startsWith function to String.', function () {
-        var input = 'this is a string';
-        expect(input.startsWith('this is')).toBeTruthy();
-        expect(input.startsWith(' a string')).toBeFalsy();
+        it('should attach a working startsWith function from ES6 to String.', function() {
+            var input = 'this is a string';
+            expect(input.startsWith('this is')).toBeTruthy();
+            expect(input.startsWith(' a string')).toBeFalsy();
+        });
     });
-
-});
+})();

0ad888b1adfda80598ad4546404853d05ec444a8
Simon Wendel <mail@simonwendel.se>
2014-12-04 16:41:19 +0100

    Reformat arrayUtility

--- client/app/scripts/services/arrayutility.js
+++ client/app/scripts/services/arrayutility.js
@@ -1,55 +1,61 @@
-'use strict';
+;(function() {
+    'use strict';
 
-angular.module('meanieBanApp')
-    .factory('arrayUtility', function () {
+    angular.module('meanieBanApp')
+        .factory('arrayUtility', arrayUtility);
 
-        function getUniqueValuesOf(property, inArray) {
-            var flags = [],
-                output = [],
-                length = inArray.length;
-
-            for (var i = 0; i < length; i++) {
-                if (flags[inArray[i][property]]) {
-                    continue;
-                }
-
-                flags[inArray[i][property]] = true;
-                output.push(inArray[i][property]);
-            }
-
-            return output;
-        }
-
-        function convert(grid, conversion) {
-            return grid.map(function (element) {
-                if(Array.isArray(element)) {
-                    return convert(element, conversion);
-                }
-
-                return conversion(element);
-            });
-        }
-
-        function get2dIndexOf(item, array) {
-            for (var i = 0; i < array.length; i++) {
-                var index = array[i].indexOf(item);
-                if (index > -1) {
-                    return [i, index];
-                }
-            }
-
-            return undefined;
-        }
-
-        function flatten(array) {
-            return [].concat.apply([], array);
-        }
-
+    function arrayUtility() {
         return {
             getUniqueValuesOf: getUniqueValuesOf,
             convert: convert,
             get2dIndexOf: get2dIndexOf,
             flatten: flatten
         };
+    }
 
-    });
+    function getUniqueValuesOf(property, inArray) {
+        var flags = [],
+            output = [],
+            length = inArray.length,
+            i;
+
+        for (i = 0; i < length; i++) {
+            if (flags[inArray[i][property]]) {
+                continue;
+            }
+
+            flags[inArray[i][property]] = true;
+            output.push(inArray[i][property]);
+        }
+
+        return output;
+    }
+
+    function convert(grid, conversion) {
+        return grid.map(function(element) {
+            if (Array.isArray(element)) {
+                return convert(element, conversion);
+            }
+
+            return conversion(element);
+        });
+    }
+
+    function get2dIndexOf(item, array) {
+        var i,
+            index;
+
+        for (i = 0; i < array.length; i++) {
+            index = array[i].indexOf(item);
+            if (index > -1) {
+                return [i, index];
+            }
+        }
+
+        return undefined;
+    }
+
+    function flatten(array) {
+        return [].concat.apply([], array);
+    }
+})();
--- client/test/spec/services/arrayutility.js
+++ client/test/spec/services/arrayutility.js
@@ -1,90 +1,89 @@
-'use strict';
-
-describe('Service: arrayUtility', function () {
-
-    beforeEach(module('meanieBanApp'));
+;(function() {
+    'use strict';
 
     var arrayUtility;
-    beforeEach(inject(function (_arrayUtility_) {
+
+    describe('Service: arrayUtility', function() {
+
+        beforeEach(module('meanieBanApp'));
+
+        beforeEach(inject(fixtureSetup));
+
+        describe('getUniqueValuesOf', function() {
+            var inArray,
+                unique;
+
+            it('should find unique values for specified property from an array of objects', function() {
+                inArray = [{blah: 'X'}, {blah: 'X'}, {blah: 'Y'}, {blah: 'Z'}, {blah: 'Y'}];
+                unique = arrayUtility.getUniqueValuesOf('blah', inArray);
+
+                expect(unique).toEqual(['X', 'Y', 'Z']);
+            });
+        });
+
+        describe('convert', function() {
+            it('should call conversion callback on every element in input grid array.', function() {
+                var converter,
+                    input,
+                    expected;
+
+                input = [
+                    [1, 2, 3],
+                    [4, 5, 6]
+                ];
+
+                expected = [
+                    ['1', '2', '3'],
+                    ['4', '5', '6']
+                ];
+
+                converter = {
+                    callback: function(element) {
+                        return element.toString();
+                    }
+                };
+
+                spyOn(converter, 'callback').andCallThrough();
+
+                expect(arrayUtility.convert(input, converter.callback)).toEqual(expected);
+                expect(converter.callback.callCount).toBe(6);
+            });
+        });
+
+        describe('get2dIndexOf', function() {
+            it('should find index of first occurrence of item in 2D array.', function() {
+                var array = [
+                    [1, 2, 3, 4],
+                    [5, 6, 7, 8],
+                    [9, 10]
+                ];
+
+                expect(arrayUtility.get2dIndexOf(7, array)).toEqual([1, 2]);
+                expect(arrayUtility.get2dIndexOf(10, array)).toEqual([2, 1]);
+                expect(arrayUtility.get2dIndexOf(19, array)).toBeUndefined();
+            });
+        });
+
+        describe('flatten', function() {
+            var array;
+
+            it('should flatten an array of several dimensions once.', function() {
+                array = [[1], [2, 3], [[4, 5], [6, 7]]];
+
+                expect(arrayUtility.flatten(array)).toEqual([1, 2, 3, [4, 5], [6, 7]]);
+            });
+
+            it('should be repeatable until an array is fully flat.', function() {
+                array = [[1], [2, 3], [[4, 5], [6, 7]]];
+
+                array = arrayUtility.flatten(array);
+
+                expect(arrayUtility.flatten(array)).toEqual([1, 2, 3, 4, 5, 6, 7]);
+            });
+        });
+    });
+
+    function fixtureSetup(_arrayUtility_) {
         arrayUtility = _arrayUtility_;
-    }));
-
-    describe('getUniqueValuesOf', function () {
-        it('should find unique values for specified property from an array of objects', function () {
-            var inArray = [
-                {
-                    blah: 'X'
-                },
-                {
-                    blah: 'X'
-                },
-                {
-                    blah: 'Y'
-                },
-                {
-                    blah: 'Z'
-                },
-                {
-                    blah: 'Y'
-                }
-            ];
-
-            var unique = arrayUtility.getUniqueValuesOf('blah', inArray);
-            expect(unique).toEqual(['X', 'Y', 'Z']);
-        });
-    });
-
-    describe('convert', function () {
-        it('should call conversion callback on every element in input grid array.', function () {
-            var converter = {
-                callback: function (element) {
-                    return element.toString();
-                }
-            };
-
-            spyOn(converter, 'callback').andCallThrough();
-
-            var input = [
-                [1, 2, 3],
-                [4, 5, 6]
-            ];
-
-            var expected = [
-                ['1', '2', '3'],
-                ['4', '5', '6']
-            ];
-
-            expect(arrayUtility.convert(input, converter.callback)).toEqual(expected);
-            expect(converter.callback.callCount).toBe(6);
-        });
-    });
-
-    describe('get2dIndexOf', function () {
-        it('should find index of first occurrence of item in 2D array.', function () {
-            var array = [
-                [1, 2, 3, 4],
-                [5, 6, 7, 8],
-                [9, 10]
-            ];
-
-            expect(arrayUtility.get2dIndexOf(7, array)).toEqual([1, 2]);
-            expect(arrayUtility.get2dIndexOf(10, array)).toEqual([2, 1]);
-            expect(arrayUtility.get2dIndexOf(19, array)).toBeUndefined();
-        });
-    });
-
-    describe('flatten', function () {
-        it('should flatten an array of several dimensions once.', function () {
-            var array = [[1], [2, 3], [[4, 5], [6, 7]]];
-            expect(arrayUtility.flatten(array)).toEqual([1, 2, 3, [4, 5], [6, 7]]);
-        });
-
-        it('should be repeatable until an array is fully flat.', function () {
-            var array = [[1], [2, 3], [[4, 5], [6, 7]]];
-
-            array = arrayUtility.flatten(array);
-            expect(arrayUtility.flatten(array)).toEqual([1, 2, 3, 4, 5, 6, 7]);
-        });
-    });
-
-});
+    }
+})();

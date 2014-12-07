90ceca7c4af4327cc3f3099f12ccd3c8cd33a5c8
Simon Wendel <mail@simonwendel.se>
2014-12-07 21:18:22 +0100

    Reformat LevelCollection

--- client/app/scripts/services/levelcollection.js
+++ client/app/scripts/services/levelcollection.js
@@ -1,69 +1,73 @@
-'use strict';
+;(function() {
+    'use strict';
 
-angular.module('meanieBanApp')
-    .service('LevelCollection',
-    ['levelData', 'arrayUtility', 'tileUtility',
-        function (levelData, arrayUtility, tileUtility) {
+    angular.module('meanieBanApp')
+        .service('LevelCollection', LevelCollection);
 
-            this.collections = collections;
+    /** @ngInject */
+    function LevelCollection(levelData, arrayUtility, tileUtility) {
 
-            this.collectionIds = collectionIds;
+        this.collections = collections;
+        this.collectionIds = collectionIds;
+        this.get = get;
 
-            this.get = get;
+        // implementation //
+        function collections() {
+            // this is O(n^2) or similar
+            var output = [],
+                names = arrayUtility.getUniqueValuesOf('collection', levelData);
 
-            // implementation //
+            names.forEach(function(name) {
+                output.push({
+                    name: name,
+                    levels: levelData
+                        .filter(function(level) {
+                            return level.collection === name;
+                        })
+                        .map(function(level) {
+                            return {id: level.id};
+                        })
 
-            function collections() {
-                // this is O(n^2) or similar
-                var output = [];
-                var names = arrayUtility.getUniqueValuesOf('collection', levelData);
-                names.forEach(function (name) {
-                    output.push({
-                        name: name,
-                        levels: levelData
-                            .filter(function (level) {
-                                return level.collection === name;
-                            })
-                            .map(function (level) {
-                                return {id: level.id};
-                            })
+                });
+            });
 
-                    });
+            return output;
+        }
+
+        function collectionIds(name) {
+            var filtered = collections()
+                .filter(function(coll) {
+                    return coll.name === name;
+                })
+                .map(function(coll) {
+                    return coll.levels;
                 });
 
+            // flatten the array
+            return arrayUtility.flatten(filtered);
+        }
+
+        function get(id) {
+            var found,
+                output;
+
+            if (isNaN(id) || parseInt(id) !== id) {
+                throw new Error('Id is not an integer.');
+            }
+
+            found = levelData.filter(function(level) {
+                return level.id === id;
+            })[0];
+
+            if (found) {
+                // copied so levelData object is not polluted with some
+                // objects having already translated rows
+                output = angular.copy(found);
+                output.rows = tileUtility.tileGridToChars(output.rows);
                 return output;
             }
 
-            function collectionIds(name) {
-                var filtered =  collections()
-                    .filter(function (coll) {
-                        return coll.name === name;
-                    })
-                    .map(function (coll) {
-                        return coll.levels;
-                    });
-
-                // flatten the array
-                return arrayUtility.flatten(filtered);
-            }
-
-            function get(id) {
-                if (isNaN(id) || parseInt(id) !== id) {
-                    throw new Error('Id is not an integer.');
-                }
-
-                var found = levelData.filter(function (level) {
-                    return level.id === id;
-                })[0];
-
-                if (found) {
-                    // copied so levelData object is not polluted with some
-                    // objects having already translated rows
-                    var output = angular.copy(found);
-                    output.rows = tileUtility.tileGridToChars(output.rows);
-                    return output;
-                }
-
-                throw new Error('No level found by that id.');
-            }
-        }]);
+            throw new Error('No level found by that id.');
+        }
+    }
+})();
--- client/test/spec/services/levelcollection.js
+++ client/test/spec/services/levelcollection.js
@@ -1,118 +1,130 @@
-'use strict';
-
-describe('Service: LevelCollection', function () {
+;(function() {
+    'use strict';
 
     var levelDataMock,
         LevelCollection,
         tileUtility;
 
-    beforeEach(
-        module('meanieBanApp', function ($provide) {
-            levelDataMock = [
-                {id: -1, width: 8, height: 1, collection: 'Coll 2', rows: [
-                    [0, 1, 2, 3, 4, 5, 6, 7]
-                ]},
-                {id: -2, width: 9, height: 9, collection: 'Coll 1', rows: [
-                    [0, 7, 7, 1, 7, 7, 7, 7, 7],
-                    [7, 7, 1, 7, 7, 1, 2, 1, 7],
-                    [7, 1, 7, 7, 1, 4, 2, 1, 7],
-                    [7, 7, 1, 4, 1, 1, 1, 7],
-                    [7, 7, 1, 4, 6, 1, 7, 7, 7],
-                    [7, 1, 4, 1, 1, 7, 7],
-                    [7, 2, 2, 1, 7, 7, 1, 7, 7],
-                    [7, 1, 1, 1, 7, 1, 7, 7],
-                    [7, 7, 7, 7, 7, 1, 7]
-                ]},
-                {id: -3, width: 8, height: 1, collection: 'Coll 1', rows: [
-                    [0, 1, 2, 3, 4, 5, 6, 7]
-                ]}
-            ];
+    describe('Service: LevelCollection', function() {
 
-            $provide.value('levelData', levelDataMock);
-        }));
+        beforeEach(module('meanieBanApp', provideSetup));
 
-    beforeEach(inject(function (_LevelCollection_, _tileUtility_) {
+        beforeEach(inject(fixtureSetup));
+
+        describe('LevelCollection.collections', function() {
+
+            it('should return all level names from the LevelCollection.', function() {
+                var collections = LevelCollection.collections();
+
+                expect(collections.length).toBe(2);
+                expect(collections[0].name).toBe('Coll 2');
+                expect(collections[1].name).toBe('Coll 1');
+            });
+
+            it('should return all level ids from the LevelCollection.', function() {
+                var collections = LevelCollection.collections();
+
+                expect(collections.length).toBe(2);
+                expect(collections[0].levels).toEqual([{id: -1}]);
+                expect(collections[1].levels).toEqual([{id: -2}, {id: -3}]);
+            });
+
+        });
+
+        describe('LevelCollection.get', function() {
+
+            it('should return a level by id.', function() {
+                var level = LevelCollection.get(-2);
+
+                expect(level).toBeDefined();
+                expect(level.id).toBe(-2);
+                expect(level.collection).toBe('Coll 1');
+            });
+
+            it('should throw exception if input id is not an integer.', function() {
+                expect(function() {
+
+                    LevelCollection.get(-3.14);
+
+                }).toThrow('Id is not an integer.');
+            });
+
+            it('should throw exception if input id is not a number.', function() {
+                expect(function() {
+
+                    LevelCollection.get('-2');
+
+                }).toThrow('Id is not an integer.');
+            });
+
+            it('should translate the raw integer tiles into character representation.', function() {
+                var level = LevelCollection.get(-2);
+
+                expect(level.rows).toEqual(tileUtility.tileGridToChars(levelDataMock[1].rows));
+            });
+
+            it('should throw exception if no level found by specified id.', function() {
+                expect(function() {
+
+                    LevelCollection.get(-8);
+
+                }).toThrow('No level found by that id.');
+            });
+
+        });
+
+        describe('LevelCollection.collectionIds', function() {
+
+            it('should return a collection of levels within the same collection.', function() {
+                var expected = [{id: -2}, {id: -3}],
+                    actual = LevelCollection.collectionIds('Coll 1');
+
+                expect(actual).toEqual(expected);
+            });
+
+            it('should return empty collection if no collection with given name.', function() {
+                var expected = [],
+                    actual = LevelCollection.collectionIds('Coll Not Found');
+
+                expect(actual).toEqual(expected);
+            });
+
+        });
+
+    });
+
+    function provideSetup($provide) {
+        levelDataMock = [
+            {
+                id: -1, width: 8, height: 1, collection: 'Coll 2', rows: [
+                [0, 1, 2, 3, 4, 5, 6, 7]
+            ]
+            },
+            {
+                id: -2, width: 9, height: 9, collection: 'Coll 1', rows: [
+                [0, 7, 7, 1, 7, 7, 7, 7, 7],
+                [7, 7, 1, 7, 7, 1, 2, 1, 7],
+                [7, 1, 7, 7, 1, 4, 2, 1, 7],
+                [7, 7, 1, 4, 1, 1, 1, 7],
+                [7, 7, 1, 4, 6, 1, 7, 7, 7],
+                [7, 1, 4, 1, 1, 7, 7],
+                [7, 2, 2, 1, 7, 7, 1, 7, 7],
+                [7, 1, 1, 1, 7, 1, 7, 7],
+                [7, 7, 7, 7, 7, 1, 7]
+            ]
+            },
+            {
+                id: -3, width: 8, height: 1, collection: 'Coll 1', rows: [
+                [0, 1, 2, 3, 4, 5, 6, 7]
+            ]
+            }
+        ];
+
+        $provide.value('levelData', levelDataMock);
+    }
+
+    function fixtureSetup(_LevelCollection_, _tileUtility_) {
         LevelCollection = _LevelCollection_;
         tileUtility = _tileUtility_;
-    }));
-
-    describe('LevelCollection.collections', function () {
-
-        it('should return all level names from the LevelCollection.', function () {
-            var collections = LevelCollection.collections();
-            expect(collections.length).toBe(2);
-            expect(collections[0].name).toBe('Coll 2');
-            expect(collections[1].name).toBe('Coll 1');
-        });
-
-        it('should return all level ids from the LevelCollection.', function () {
-            var collections = LevelCollection.collections();
-            expect(collections.length).toBe(2);
-            expect(collections[0].levels).toEqual([
-                { id: -1}
-            ]);
-            expect(collections[1].levels).toEqual([
-                { id: -2},
-                { id: -3 }
-            ]);
-        });
-
-    });
-
-    describe('LevelCollection.get', function () {
-
-        it('should return a level by id.', function () {
-            var level = LevelCollection.get(-2);
-            expect(level).toBeDefined();
-            expect(level.id).toBe(-2);
-            expect(level.collection).toBe('Coll 1');
-        });
-
-        it('should throw exception if input id is not an integer.', function () {
-            expect(function () {
-
-                LevelCollection.get(-3.14);
-
-            }).toThrow('Id is not an integer.');
-        });
-
-        it('should throw exception if input id is not a number.', function () {
-            expect(function () {
-
-                LevelCollection.get('-2');
-
-            }).toThrow('Id is not an integer.');
-        });
-
-        it('should translate the raw integer tiles into character representation.', function () {
-            var level = LevelCollection.get(-2);
-            expect(level.rows).toEqual(tileUtility.tileGridToChars(levelDataMock[1].rows));
-        });
-
-        it('should throw exception if no level found by specified id.', function () {
-            expect(function () {
-
-                LevelCollection.get(-8);
-
-            }).toThrow('No level found by that id.');
-        });
-
-    });
-
-    describe('LevelCollection.collectionIds', function () {
-
-        it('should return a collection of levels within the same collection.', function () {
-            var expected = [{ id: -2}, { id: -3}];
-            var actual = LevelCollection.collectionIds('Coll 1');
-            expect(actual).toEqual(expected);
-        });
-
-        it('should return empty collection if no collection with given name.', function () {
-            var expected = [];
-            var actual = LevelCollection.collectionIds('Coll Not Found');
-            expect(actual).toEqual(expected);
-        });
-
-    });
-
-});
+    }
+})();

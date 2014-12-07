26c4f9cc75383117b0e434f8198544b2fb1de083
Simon Wendel <mail@simonwendel.se>
2014-12-07 21:22:21 +0100

    Reformat smallestSolvable test utility

--- client/test/utilities/smallestsolvable.js
+++ client/test/utilities/smallestsolvable.js
@@ -1,13 +1,17 @@
-'use strict';
+;(function() {
+    'use strict';
 
-// this is only to be used in tests, so the same grid doesn't need to
-// be repeated over and over. it is the smallest solvable level that
-// can be produced in Sokoban.
-angular.module('meanieBanApp')
-    .factory('smallestSolvable', function (tileUtility) {
+    /*this is only to be used in tests, so the same grid doesn't need to
+     be repeated over and over. it is the smallest solvable level that
+     can be produced in Sokoban.*/
+    angular.module('meanieBanApp')
+        .factory('smallestSolvable', smallestSolvable);
+
+    function smallestSolvable(tileUtility) {
         return tileUtility.stringGridToChars([
             ['wall', 'wall', 'wall', 'wall', 'wall'],
             ['wall', 'dock', 'box', 'worker', 'wall'],
             ['wall', 'wall', 'wall', 'wall', 'wall']
         ]);
-    });
+    }
+})();

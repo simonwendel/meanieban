b5bf13a67e6794f52177d28e99fa583694e15437
Simon Wendel <mail@simonwendel.se>
2014-12-07 21:48:17 +0100

    Extract functions out of factory in levelCollection

--- client/app/scripts/services/levelcollection.js
+++ client/app/scripts/services/levelcollection.js
@@ -6,70 +6,76 @@
         .factory('levelCollection', levelCollection);
 
     /** @ngInject */
-    function levelCollection(levelData, arrayUtility, tileUtility) {
+    function levelCollection(_levelData_, _arrayUtility_, _tileUtility_) {
+        levelData = _levelData_;
+        arrayUtility = _arrayUtility_;
+        tileUtility = _tileUtility_;
+
         return {
             collections: collections,
             collectionIds: collectionIds,
             get: get
         };
+    }
 
-        // implementation //
-        function collections() {
-            // this is O(n^2) or similar
-            var output = [],
-                names = arrayUtility.getUniqueValuesOf('collection', levelData);
+    var levelData,
+        arrayUtility,
+        tileUtility;
 
-            names.forEach(function(name) {
-                output.push({
-                    name: name,
-                    levels: levelData
-                        .filter(function(level) {
-                            return level.collection === name;
-                        })
-                        .map(function(level) {
-                            return {id: level.id};
-                        })
+    function collections() {
+        // this is O(n^2) or similar
+        var output = [],
+            names = arrayUtility.getUniqueValuesOf('collection', levelData);
 
-                });
+        names.forEach(function(name) {
+            output.push({
+                name: name,
+                levels: levelData
+                    .filter(function(level) {
+                        return level.collection === name;
+                    })
+                    .map(function(level) {
+                        return {id: level.id};
+                    })
+            });
+        });
+
+        return output;
+    }
+
+    function collectionIds(name) {
+        var filtered = collections()
+            .filter(function(coll) {
+                return coll.name === name;
+            })
+            .map(function(coll) {
+                return coll.levels;
             });
 
+        // flatten the array
+        return arrayUtility.flatten(filtered);
+    }
+
+    function get(id) {
+        var found,
+            output;
+
+        if (isNaN(id) || parseInt(id) !== id) {
+            throw new Error('Id is not an integer.');
+        }
+
+        found = levelData.filter(function(level) {
+            return level.id === id;
+        })[0];
+
+        if (found) {
+            // copied so levelData object is not polluted with some
+            // objects having already translated rows
+            output = angular.copy(found);
+            output.rows = tileUtility.tileGridToChars(output.rows);
             return output;
         }
 
-        function collectionIds(name) {
-            var filtered = collections()
-                .filter(function(coll) {
-                    return coll.name === name;
-                })
-                .map(function(coll) {
-                    return coll.levels;
-                });
-
-            // flatten the array
-            return arrayUtility.flatten(filtered);
-        }
-
-        function get(id) {
-            var found,
-                output;
-
-            if (isNaN(id) || parseInt(id) !== id) {
-                throw new Error('Id is not an integer.');
-            }
-
-            found = levelData.filter(function(level) {
-                return level.id === id;
-            })[0];
-
-            if (found) {
-                // copied so levelData object is not polluted with some
-                // objects having already translated rows
-                output = angular.copy(found);
-                output.rows = tileUtility.tileGridToChars(output.rows);
-                return output;
-            }
-
-            throw new Error('No level found by that id.');
-        }
+        throw new Error('No level found by that id.');
     }
 })();

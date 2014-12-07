59560f53e7d688756f977b8b41940c06924143d0
Simon Wendel <mail@simonwendel.se>
2014-12-07 19:35:42 +0100

    Reformat Level

--- client/app/scripts/services/level.js
+++ client/app/scripts/services/level.js
@@ -1,90 +1,92 @@
-'use strict';
+;(function() {
+    'use strict';
 
-angular.module('meanieBanApp')
-    .factory('Level',
-    ['arrayUtility', 'tileUtility', 'Worker',
-        function (arrayUtility, tileUtility, Worker) {
+    angular.module('meanieBanApp')
+        .factory('Level', Level);
 
-            return function (grid) {
-                if (!grid) {
-                    throw new Error('Parameter grid to constructor function cannot be undefined.');
+    /** @ngInject */
+    function Level(arrayUtility, tileUtility, Worker) {
+        return function(grid) {
+            if (!grid) {
+                throw new Error('Parameter grid to constructor function cannot be undefined.');
+            }
+
+            this.grid = getGrid;
+            this.worker = getWorker;
+            this.update = update;
+            this.inspect = inspect;
+
+            // implementation //
+            var gridArray = grid,
+                location = calculateWorkerLocation(gridArray),
+                worker = new Worker(location.x, location.y);
+
+            function getGrid() {
+                return gridArray;
+            }
+
+            function getWorker() {
+                return worker;
+            }
+
+            function update(x, y, state) {
+                if (!tileUtility.isValidChar(state)) {
+                    throw new Error('Cannot set invalid state in grid.');
                 }
 
-                this.grid = getGrid;
-
-                this.worker = getWorker;
-
-                this.update = update;
-
-                this.inspect = inspect;
-
-                // implementation //
-
-                var gridArray = grid;
-
-                function getGrid() {
-                    return gridArray;
+                if (!gridArray[y] || !gridArray[y][x]) {
+                    throw new Error('Coordinates out of bounds on level grid.');
                 }
 
-                var location = calculateWorkerLocation(gridArray);
-                var worker = new Worker(location.x, location.y);
+                gridArray[y][x] = state;
+            }
 
-                function getWorker() {
-                    return worker;
-                }
+            function inspect(strategy) {
+                var r,
+                    row,
+                    c,
+                    cell;
 
-                function update(x, y, state) {
-                    if (!tileUtility.isValidChar(state)) {
-                        throw new Error('Cannot set invalid state in grid.');
-                    }
-
-                    if (!gridArray[y] || !gridArray[y][x]) {
-                        throw new Error('Coordinates out of bounds on level grid.');
-                    }
-
-                    gridArray[y][x] = state;
-                }
-
-                function inspect(strategy) {
-                    // O(n) over number of cells, worst
-                    // case if strategy returns false for all
-                    for (var r = 0; r < gridArray.length; r++) {
-                        var row = gridArray[r];
-                        for (var c = 0; c < row.length; c++) {
-                            var cell = row[c];
-                            if (strategy(cell)) {
-                                return true;
-                            }
+                // O(n) over number of cells, worst
+                // case if strategy returns false for all
+                for (r = 0; r < gridArray.length; r++) {
+                    row = gridArray[r];
+                    for (c = 0; c < row.length; c++) {
+                        cell = row[c];
+                        if (strategy(cell)) {
+                            return true;
                         }
                     }
-
-                    return false;
-                }
-            };
-
-            function calculateWorkerLocation(gridArray) {
-                var index = searchTiles('worker', gridArray);
-                if (index) {
-                    return index;
                 }
 
-                index = searchTiles('worker-docked', gridArray);
-                if (index) {
-                    return index;
-                }
+                return false;
+            }
+        };
 
-                throw new Error('Invalid level, no worker location found.');
+        function calculateWorkerLocation(gridArray) {
+            var index = searchTiles('worker', gridArray);
+            if (index) {
+                return index;
             }
 
-            function searchTiles(tileString, gridArray) {
-                var worker = tileUtility.stringToChar(tileString);
-                var index = arrayUtility.get2dIndexOf(worker, gridArray);
-                if (index) {
-                    return {
-                        x: index[1],
-                        y: index[0]
-                    };
-                }
+            index = searchTiles('worker-docked', gridArray);
+            if (index) {
+                return index;
             }
 
-        }]);
+            throw new Error('Invalid level, no worker location found.');
+        }
+
+        function searchTiles(tileString, gridArray) {
+            var worker = tileUtility.stringToChar(tileString),
+                index = arrayUtility.get2dIndexOf(worker, gridArray);
+
+            if (index) {
+                return {
+                    x: index[1],
+                    y: index[0]
+                };
+            }
+        }
+    }
+})();
--- client/test/spec/services/level.js
+++ client/test/spec/services/level.js
@@ -1,112 +1,124 @@
-'use strict';
+;(function() {
+    'use strict';
 
-describe('Service: Level', function () {
+    var Level,
+        gridArray,
+        tileUtility;
 
-    beforeEach(module('meanieBanApp'));
+    describe('Service: Level', function() {
 
-    var Level, gridArray, tileUtility;
-    beforeEach(inject(function (_Level_, _tileUtility_, smallestSolvable) {
+        beforeEach(module('meanieBanApp'));
+
+        beforeEach(inject(fixtureSetup));
+
+        it('should be constructable from an array of integers.', function() {
+            var level = new Level(gridArray);
+
+            expect(level).toBeDefined();
+            expect(level.grid()).toEqual(gridArray);
+        });
+
+        it('should throw exception when grid is undefined to constructor.', function() {
+            expect(function() {
+
+                new Level();
+
+            }).toThrow('Parameter grid to constructor function cannot be undefined.');
+        });
+
+        it('should throw exception if no worker location is found in the Level on construction.', function() {
+            expect(function() {
+
+                new Level(tileUtility.stringGridToChars([
+                    ['wall']
+                ]));
+
+            }).toThrow('Invalid level, no worker location found.');
+        });
+
+        describe('worker', function() {
+            it('should return an object of type Worker.', inject(function(Worker) {
+                var level = new Level(gridArray);
+
+                expect(level.worker() instanceof Worker).toBeTruthy();
+            }));
+
+            it('should be able to get worker location in the Level.', function() {
+                var level = new Level(gridArray);
+
+                expect(level.worker().location).toEqual({x: 3, y: 1});
+            });
+
+            it('should be able to get docked worker location in the Level.', function() {
+                var level = new Level(tileUtility.stringGridToChars([
+                    ['worker-docked']
+                ]));
+
+                expect(level.worker().location).toEqual({x: 0, y: 0});
+            });
+        });
+
+        describe('update', function() {
+            it('should throw exception if the co-ordinates are out of bounds on the grid.', function() {
+                var level = new Level(gridArray);
+                spyOn(tileUtility, 'isValidChar').andReturn(true);
+
+                expect(function() {
+                    level.update(-1, -1, 'tile');
+                }).toThrow('Coordinates out of bounds on level grid.');
+
+                expect(function() {
+                    level.update(2, 5, 'tile');
+                }).toThrow('Coordinates out of bounds on level grid.');
+            });
+
+            it('should throw exception if the new state is not valid.', function() {
+                var level = new Level(gridArray);
+                spyOn(tileUtility, 'isValidChar').andReturn(false);
+
+                expect(function() {
+                    level.update(-1, -1, 'tile');
+                }).toThrow('Cannot set invalid state in grid.');
+                expect(tileUtility.isValidChar.callCount).toBe(1);
+            });
+
+            it('should be able to set a new valid state in the Level grid.', function() {
+                var level = new Level(gridArray);
+                spyOn(tileUtility, 'isValidChar').andReturn(true);
+
+                level.update(1, 1, 'tile');
+
+                expect(level.grid()[1][1]).toEqual('tile');
+                expect(tileUtility.isValidChar.callCount).toBe(1);
+            });
+        });
+
+        describe('inspect', function() {
+            it('should run a strategy on all tiles until the strategy returns true and then return true.', function() {
+                var level = new Level(gridArray),
+                    strategy = jasmine.createSpy('strategy').andCallFake(
+                        function(cell) {
+                            return cell === tileUtility.stringToChar('worker');
+                        });
+
+                expect(level.inspect(strategy)).toBeTruthy();
+                expect(strategy.callCount).toBe(9);
+            });
+
+            it('should run the strategy on all tiles and return false if the strategy returns false for all.', function() {
+                var level = new Level(gridArray),
+                    strategy = jasmine.createSpy('strategy').andReturn(false);
+
+                expect(level.inspect(strategy)).toBeFalsy();
+                expect(strategy.callCount).toBe(15);
+            });
+        });
+
+    });
+
+    function fixtureSetup(_Level_, _tileUtility_, smallestSolvable) {
         Level = _Level_;
         gridArray = smallestSolvable;
         tileUtility = _tileUtility_;
-    }));
-
-    it('should be constructable from an array of integers.', function () {
-        var level = new Level(gridArray);
-        expect(level).toBeDefined();
-        expect(level.grid()).toEqual(gridArray);
-    });
-
-    it('should throw exception when grid is undefined to constructor.', function () {
-        expect(function () {
-
-            new Level();
-
-        }).toThrow('Parameter grid to constructor function cannot be undefined.');
-    });
-
-    it('should throw exception if no worker location is found in the Level on construction.', function () {
-        expect(function () {
-
-            new Level(tileUtility.stringGridToChars([
-                ['wall']
-            ]));
-
-        }).toThrow('Invalid level, no worker location found.');
-    });
-
-    describe('worker', function () {
-        it('should return an object of type Worker.', inject(function (Worker) {
-            var level = new Level(gridArray);
-            expect(level.worker() instanceof Worker).toBeTruthy();
-        }));
-
-        it('should be able to get worker location in the Level.', function () {
-            var level = new Level(gridArray);
-            expect(level.worker().location).toEqual({x: 3, y: 1});
-        });
-
-        it('should be able to get docked worker location in the Level.', function () {
-            var level = new Level(tileUtility.stringGridToChars([
-                ['worker-docked']
-            ]));
-            expect(level.worker().location).toEqual({x: 0, y: 0});
-        });
-    });
-
-    describe('update', function () {
-        it('should throw exception if the co-ordinates are out of bounds on the grid.', function () {
-            spyOn(tileUtility, 'isValidChar').andReturn(true);
-            var level = new Level(gridArray);
-
-            expect(function () {
-                level.update(-1, -1, 'tile');
-            }).toThrow('Coordinates out of bounds on level grid.');
-
-            expect(function () {
-                level.update(2, 5, 'tile');
-            }).toThrow('Coordinates out of bounds on level grid.');
-        });
-
-        it('should throw exception if the new state is not valid.', function () {
-            spyOn(tileUtility, 'isValidChar').andReturn(false);
-            var level = new Level(gridArray);
-
-            expect(function () {
-                level.update(-1, -1, 'tile');
-            }).toThrow('Cannot set invalid state in grid.');
-            expect(tileUtility.isValidChar.callCount).toBe(1);
-        });
-
-        it('should be able to set a new valid state in the Level grid.', function () {
-            spyOn(tileUtility, 'isValidChar').andReturn(true);
-            var level = new Level(gridArray);
-
-            level.update(1, 1, 'tile');
-
-            expect(level.grid()[1][1]).toEqual('tile');
-            expect(tileUtility.isValidChar.callCount).toBe(1);
-        });
-    });
-
-    describe('inspect', function () {
-        it('should run a strategy on all tiles until the strategy returns true and then return true.', function () {
-            var strategy = jasmine.createSpy('strategy').andCallFake(
-                function (cell) {
-                    return cell === tileUtility.stringToChar('worker');
-                });
-
-            var level = new Level(gridArray);
-            expect(level.inspect(strategy)).toBeTruthy();
-            expect(strategy.callCount).toBe(9);
-        });
-
-        it('should run the strategy on all tiles and return false if the strategy returns false for all.', function () {
-            var strategy = jasmine.createSpy('strategy').andReturn(false);
-            var level = new Level(gridArray);
-            expect(level.inspect(strategy)).toBeFalsy();
-            expect(strategy.callCount).toBe(15);
-        });
-    });
-
-});
+    }
+})();

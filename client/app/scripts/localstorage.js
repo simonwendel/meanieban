0d2682069d22650f6f433d04db9b1cfdf507e23d
Simon Wendel <mail@simonwendel.se>
2015-01-07 14:15:57 +0100

    Change cookie timeout to 30 days

--- client/app/scripts/localstorage.js
+++ client/app/scripts/localstorage.js
@@ -5,12 +5,18 @@
         .module('meanieBanApp')
         .config(localStorageSetup);
 
+    /***
+     * The number of days for the cookie to live, 0 = infinity.
+     * @type {number}
+     */
+    var cookieDuration = 30;
+
     /** @ngInject */
     function localStorageSetup(localStorageServiceProvider) {
         localStorageServiceProvider
             .setPrefix('meanieBan')
             .setStorageType('localStorage')
-            .setStorageCookie(0, '/')
+            .setStorageCookie(cookieDuration, '/')
             .setNotify(true, true);
     }
 })();
